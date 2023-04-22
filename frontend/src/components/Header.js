598ca7a878d8cad44c351203af1996544cbe1381
Twwan <65574497+Twwan@users.noreply.github.com>
2023-04-22 14:12:28 +0700

    fixed links

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,11 +1,10 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <Link to={`/`}>
+    <a href={`/`}>
       <h1>NOTEWIT</h1>
-    </Link>
+    </a>
   );
 };
 
--- frontend/src/components/ListItem.js
+++ frontend/src/components/ListItem.js
@@ -1,15 +1,10 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 const ListItem = ({ note }) => {
   return (
-    <div>
-      <Link to={`/note/${note.id}`}>
-        <h3>{note.title}</h3>
-      </Link>
-      <p>{note.body}</p>
-      <br />
-    </div>
+    <a href={`/note/${note.id}`}>
+      <h3>{note.title}</h3>
+    </a>
   );
 };
 
