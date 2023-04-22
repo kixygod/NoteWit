fcb621e7753026ae23b4c3aef75a3f3e82a32fbe
Bogdan Eremin <03bogdan80@gmail.com>
2023-04-22 10:51:29 +0700

    Time zone changed to Tomsk

--- api/models.py
+++ api/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 
 class Note(models.Model):
+    title = models.TextField(null=True, blank=True)
     body = models.TextField(null=True, blank=True)
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
